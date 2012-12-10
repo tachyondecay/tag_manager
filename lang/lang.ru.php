45a6e98441ff32f0b9ba3d7bd023906f6dd34a70
alexbirukov <lord_maloy@mail.ru>
2012-12-10 23:27:27 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,60 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2012-08-06'
+	);
+
+	/**
+	 * Tag Manager
+	 */
+	$dictionary = array(
+
+		'Count' => 
+		'Количество',
+
+		'Frequency' => 
+		'Упоминания',
+
+		'No Appropriate Tag Fields' => 
+		'Нет соответствующих полей тегов',
+
+		'No Tag Field Found' => 
+		'Поля тегов не найдены',
+
+		'No taglist field with the specified ID was found.' => 
+		'Поле тегов с указаным ID не найдено.',
+
+		'Section' => 
+		'Раздел',
+
+		'Section List' => 
+		'Список разделов',
+
+		'Tag Field' => 
+		'Поле тегов',
+
+		'Tag Manager' => 
+		'Управление тегами',
+
+		'Taglist Fields' => 
+		'Поля тегов',
+
+		'Tags' => 
+		'Теги',
+
+		'Tags successfully ' => 
+		'Теги успешно ',
+
+		'The Section you are looking for, <code>%s</code>, could not be found.' => 
+		'Указанный раздел <code>%s</code> не найден.',
+
+		'The Section you specified, <code>%s</code>, does not have any taglist fields.' => 
+		'Указанный раздел <code>%s</code> не имеет полей тегов.',
+
+	);
