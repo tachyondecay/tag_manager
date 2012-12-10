c844bc01ba9313d2073cd93804b116766f4486d7
alexbirukov <lord_maloy@mail.ru>
2012-12-11 01:11:00 +0600

    Update Russian language
    Update Russian language file

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,7 +7,7 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-08-06'
+		'release-date' => '2012-12-11'
 	);
 
 	/**
@@ -56,5 +56,68 @@
 
 		'The Section you specified, <code>%s</code>, does not have any taglist fields.' => 
 		'Указанный раздел <code>%s</code> не имеет полей тегов.',
+
+		'A front-end user' => 
+		'Фронт-энд пользователь',
+
+		'An unidentified user (%s)' => 
+		'Неопределённый пользователь (%s)',
+
+		'%1$s deleted %2$s.' => 
+		'%1$s удалено %2$s.',
+
+		'%1$s updated %2$s.' => 
+		'%1$s обновлено %2$s.',
+
+		'%1$s created %2$s.' => 
+		'%1$s создано %2$s.',
+
+		'%1$s enabled %2$s.' => 
+		'%1$s включено %2$s.',
+
+		'%1$s disabled %2$s.' => 
+		'%1$s отключено %2$s.',
+
+		'%1$s logged in %2$s.' => 
+		'%1$s залогинено %2$s.',
+
+		'%1$s attempted to log in %2$s.' => 
+		'%1$s пытались залогиниться %2$s.',
+
+		'%1$s reset %2$s.' => 
+		'%1$s сброшено %2$s.',
+
+		'%1$s attempted to reset %2$s.' => 
+		'%1$s пытались сбросить %2$s.',
+
+		'%1$s changed %2$s.' => 
+		'%1$s изменено %2$s.' ,
+
+		'%1$s requested to reset %2$s.' => 
+		'%1$s необходимо сбросить %2$s.',
+
+		'%1$s uninstalled %2$s.' => 
+		'%1$s удалено %2$s.',
+
+		'%1$s %2$s %3$s.' => 
+		'%1$s %2$s %3$s.' ,
+
+		'template' => 
+		'шаблон',
+
+		'author record' => 
+		'хапись автора',
+
+		'system preferences' => 
+		'системные настройки',
+
+		' maintenance mode' => 
+		' режим обслуживания',
+
+		' to the back end' => 
+		' в бэк-энд',
+
+		' his/her password' => 
+		' его/её пароль',
 
 	);
